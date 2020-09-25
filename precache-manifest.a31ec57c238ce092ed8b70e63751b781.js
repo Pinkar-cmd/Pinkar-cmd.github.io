2fe167e33881ca4ad00b423ecd1791cc7e9df47c
pinkar.kumari <pinkar.kumari@diaspark.com>
2020-09-25 17:17:53 +0530

    initial commit

---
+++ precache-manifest.a31ec57c238ce092ed8b70e63751b781.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbac1a8a917238f7a716",
+    "url": "/static/css/main.b030597b.chunk.css"
+  },
+  {
+    "revision": "fbac1a8a917238f7a716",
+    "url": "/static/js/main.05098872.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "383c80dd1675db666545",
+    "url": "/static/css/2.b70df8cb.chunk.css"
+  },
+  {
+    "revision": "383c80dd1675db666545",
+    "url": "/static/js/2.e148671d.chunk.js"
+  },
+  {
+    "revision": "930b558dccdcc520487e0c10f0e33f41",
+    "url": "/static/media/modern-design.930b558d.jpg"
+  },
+  {
+    "revision": "0f9282314a8dc69f139065112fbf176d",
+    "url": "/static/media/clean-design.0f928231.jpg"
+  },
+  {
+    "revision": "b6918a9f3c3b56d411df653376890176",
+    "url": "/static/media/great-support.b6918a9f.jpg"
+  },
+  {
+    "revision": "80fba52bc4f43742fd980e82a4a99a5a",
+    "url": "/static/media/easy-customise.80fba52b.jpg"
+  },
+  {
+    "revision": "7e97e67b026423883253a8575747f128",
+    "url": "/static/media/unlimited-features.7e97e67b.jpg"
+  },
+  {
+    "revision": "c294df00f272aa634095929801de3ec0",
+    "url": "/static/media/advanced-option.c294df00.jpg"
+  },
+  {
+    "revision": "158cf4042a06c345e3058e7e43755ce2",
+    "url": "/static/media/bg-hero.158cf404.jpg"
+  },
+  {
+    "revision": "88149694961edb70271403615f5289a0",
+    "url": "/static/media/bg-works.88149694.jpg"
+  },
+  {
+    "revision": "b5ee6cb6040e04e55f11a0fdd729b72d",
+    "url": "/index.html"
+  }
+];
